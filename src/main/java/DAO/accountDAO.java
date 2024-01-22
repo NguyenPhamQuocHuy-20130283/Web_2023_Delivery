6bf2eb5791d3a259cb5c3517b6e79ec7853770b0
hungdang2905 <20130271@st.hcmuaf.edu.vn>
2024-01-23 03:52:07 +0700

    AccountDao getAllAccount

---
+++ src/main/java/DAO/accountDAO.java
@@ -0,0 +1,35 @@
+package DAO;
+
+import Connect.Connect;
+import model.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class accountDAO {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Account> getAllAccount() {
+        List<Account> list = new ArrayList<>();
+        String query = "select * from User";
+        try {
+            conn = new Connect().getDBConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Account(rs.getInt(1),
+                        rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getString(6),rs.getString(7)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+
+    }
+}
