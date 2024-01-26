0a0235477d12485d4f6a01f260ea054d4c1b92f3
NguyenPhamQuocHuy-20130283 <20130283@st.hcmuaf.edu.vn>
2024-01-26 13:23:40 +0700

    forget password

--- src/main/java/DAO/Client/AccountDAO.java
+++ src/main/java/DAO/Client/AccountDAO.java
@@ -1,7 +1,13 @@
 package DAO.Client;
 
 import Connect.DBConnect;
+import Utils.EnCode;
+import model.Account;
 import org.jdbi.v3.core.Jdbi;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class AccountDAO {
     public AccountDAO() {
@@ -16,6 +22,54 @@ public class AccountDAO {
         } catch (Exception e) {
             e.printStackTrace();
             return true;
+        }
+    }
+    //get 1 account by email
+    public static Account getAccountByEmail(String email) {
+
+        String query = "select idCustomer,userName,password,Name,Address,Email,NumberPhone,id_role_member from customer where Email = ?";
+        try (Connection conn = DBConnect.getConnection(); PreparedStatement ps = conn.prepareStatement(query);) {
+            ps.setString(1, email);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    return new Account(rs.getInt("idCustomer"), rs.getString("userName"), rs.getString("password"),
+                            rs.getString("Name"), rs.getString("Address"), rs.getString("Email"),
+                            rs.getString("NumberPhone"), rs.getInt("id_role_member"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    public static Account checkAccountExistByid(String uid) {
+
+        String query = "select idCustomer,userName,password,Name,Address,Email,NumberPhone,id_role_member from customer where idCustomer = ?";
+        try (Connection conn = DBConnect.getConnection(); PreparedStatement ps = conn.prepareStatement(query);) {
+            ps.setString(1, uid);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    return new Account(rs.getInt("idCustomer"), rs.getString("userName"), rs.getString("password"),
+                            rs.getString("Name"), rs.getString("Address"), rs.getString("Email"),
+                            rs.getString("NumberPhone"), rs.getInt("id_role_member"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    public static void editAccountPassword(String uid, String newpass) {
+        String query = "update idCustomer set password = ? where idCustomer = ?;";
+        newpass = EnCode.toSHA1(newpass);
+        try (Connection conn = DBConnect.getConnection(); PreparedStatement ps = conn.prepareStatement(query);) {
+
+            ps.setString(1, newpass);
+            ps.setString(2, uid);
+            ps.executeUpdate();
+        } catch (Exception e) {
         }
     }
 }
