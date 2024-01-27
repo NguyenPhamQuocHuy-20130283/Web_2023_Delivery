62e0d7a9bf7fd691beacc3e7a0150d70e9bcbf89
hungdang2905 <20130271@st.hcmuaf.edu.vn>
2024-01-27 19:16:59 +0700

    DAO account va AccountMapper

--- src/main/java/DAO/admin/AccountDAO.java
+++ src/main/java/DAO/admin/AccountDAO.java
@@ -5,90 +5,72 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import Connect.DBConnect;
+import mapper.AccountMapper;
 import model.Account;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.Update;
 
 public class AccountDAO {
     public static List<Account> getListAccount() {
         ArrayList<Account> list = new ArrayList<>();
         String query = "select * from Account  ";
-        try {
-            Connection conn = DBConnect.getConnection();
-            PreparedStatement ps = conn.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                list.add(new Account(rs.getInt("idCustomer"), rs.getString("userName"), rs.getString("password"),
-                        rs.getString("Name"), rs.getString("Address"), rs.getString("Email"),
-                        rs.getString("NumberPhone"), rs.getInt("id_role_member")));
-            }
-        } catch (Exception e) {
-        }
+
+            Jdbi me = DBConnect.me();
+            list= (ArrayList<Account>) me.withHandle(handle -> handle.createQuery(query).map(new AccountMapper()).list());
+
+
         return list;
     }
 
     public static Account getAccountById(String uid) {
         String query = "select * from Account where uid = ? ";
-        try {
-            Connection conn = DBConnect.getConnection();
-            PreparedStatement ps = conn.prepareStatement(query);
-            ps.setString(1, uid);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                return new Account(rs.getInt("idCustomer"), rs.getString("userName"), rs.getString("password"),
-                        rs.getString("Name"), rs.getString("Address"), rs.getString("Email"),
-                        rs.getString("NumberPhone"), rs.getInt("id_role_member"));
-            }
-        } catch (Exception e) {
-        }
-        return null;
+        Jdbi me = DBConnect.me();
+        return me.withHandle(handle -> handle.createQuery("query")
+                .bind(0, uid)
+                .map(new AccountMapper())
+                .findOne()
+                .orElse(null));
     }
 
     public static void removeAccount(String uid) {
         String query = "DELETE FROM Account WHERE uid = ?";
-        try {
-            Connection conn = DBConnect.getConnection();
-            PreparedStatement ps = conn.prepareStatement(query);
-            ps.setString(1, uid);
-            ps.executeUpdate();
-        } catch (Exception e) {
-
-        }
+        Jdbi me = DBConnect.me();
+        me.useHandle(handle -> handle.createUpdate(query)
+                .bind(0, uid)
+                .execute());
     }
     public static void insertAccount( Account account) {
         String insertQuery = "INSERT INTO Account (accountName, password, fullName, address, email, phone, idRoleMember) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        try {
-            Connection conn = DBConnect.getConnection();
-            PreparedStatement ps = conn.prepareStatement(insertQuery);
-            ps.setString(1, account.getAccountName());
-            ps.setString(2, account.getPassword());
-            ps.setString(3, account.getFullName());
-            ps.setString(4, account.getAddress());
-            ps.setString(5, account.getEmail());
-            ps.setString(6, account.getPhone());
-            ps.setInt(7, account.getIdRoleMember());
-            ps.executeUpdate();
-        } catch (Exception e) {
+        Jdbi me = DBConnect.me();
+        me.useTransaction(handle -> {
+            handle.createUpdate(insertQuery)
+                    .bind(0, account.getAccountName())
+                    .bind(1, account.getPassword())
+                    .bind(2, account.getFullName())
+                    .bind(3, account.getAddress())
+                    .bind(4, account.getEmail())
+                    .bind(5, account.getPhone())
+                    .bind(6, account.getIdRoleMember())
+                    .execute();
+        });
 
-        }
     }
 
 
     	public static void updateAccount(Account account) {
-
+            Jdbi me = DBConnect.me();
 	String query = "update Account set accountName = ?, password =?,email=?,address=?,phoneNumber=?,idRoleMember=? where uid =?;";
-		try {
-			Connection conn = DBConnect.getConnection();
-			PreparedStatement ps = conn.prepareStatement(query);
-			ps.setString(1, account.getAccountName());
-			ps.setString(2, account.getPassword());
-			ps.setString(3, account.getEmail());
-			ps.setString(4, account.getAddress());
-			ps.setString(5, account.getPhone());
-            ps.setInt(6, account.getIdRoleMember());
-			ps.setInt(7, account.getId());
-		ps.executeUpdate();
-		} catch (Exception e) {
-
-		}
+            me.useTransaction(handle -> {
+                Update updateAccount = handle.createUpdate(query);
+                updateAccount.bind("accountName", account.getAccountName())
+                        .bind("password", account.getPassword())
+                        .bind("email", account.getEmail())
+                        .bind("address", account.getAddress())
+                        .bind("phoneNumber", account.getPhone())
+                        .bind("idRoleMember", account.getIdRoleMember())
+                        .bind("uid", account.getId())
+                        .execute();
+            });
 	}
 
     public static void main(String[] args) {
